2475d340a82af854025f511d2af1b2a3f28c4916
bongiwentshangase <bongiwe.buselaphi@gmail.com>
2020-07-29 08:24:59 +0200

    variables

---
+++ js/variables/exercises.js
@@ -0,0 +1,47 @@
+1.// swap the values of 2 variables
+let b1=3;
+let b2=7;
+let swap;
+
+swap=b1;
+b1=b2;
+b2=swap;
+
+
+
+
+
+
+
+// 2.write the values of x , y and z after each line.
+
+let x =3;
+let y =5;
+let z =7;
+
+z=y;
+y = z * x;
+x = x+2;
+
+x++;
+
+
+3.//if else
+
+let time="afternoon"
+
+
+if (time=="night") {
+
+    console.log("goodnight")  
+
+}
+else(
+
+    console.log("goodmorning")
+
+)
+
+console.log("the end")
+
+4.
